97a032f0ca98faa6eb529294bba6f59f001a5bb1
bricdocs <bricdocs.web@gmail.com>
2026-07-08 23:04:50 +0300

    Add camera functionality to access environment camera
    Implement camera access using getUserMedia API.

--- js/camera.js
+++ js/camera.js
@@ -1,1 +1,43 @@
+const video=document.getElementById("video");
 
+
+async function startCamera(){
+
+
+try{
+
+
+const stream =
+await navigator.mediaDevices.getUserMedia({
+
+video:{
+
+facingMode:"environment",
+
+width:1280,
+
+height:720
+
+}
+
+});
+
+
+video.srcObject=stream;
+
+
+}
+
+catch(e){
+
+alert(
+"Kamera açılamadı: "+e
+);
+
+}
+
+
+}
+
+
+startCamera();
